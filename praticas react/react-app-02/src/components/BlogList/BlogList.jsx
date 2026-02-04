eecb7324e48368fb51480d43c6a90acaeccd9d7f
Alice <alicemariadelimamelo@gmail.com>
2026-02-03 22:52:28 -0300

    feat: ajustando o alinhamento do perfil e lista de cards .

--- praticas react/react-app-02/src/components/BlogList/BlogList.jsx
+++ praticas react/react-app-02/src/components/BlogList/BlogList.jsx
@@ -6,15 +6,21 @@ function BlogList() {
     <section className="blog-list">
       <h2>Últimas do blog</h2>
 
-      <BlogCard
-        titulo="5 dicas para sua carreira profissional"
-        descricao="Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end"
-      />
+    <BlogCard
+      titulo="Interpretando as Profecias do Oráculo"
+      descricao="Entenda por que você nunca deve levar as palavras do Oráculo de Delfos ao pé da letra se quiser sobreviver à sua missão."
+    />
 
-      <BlogCard
-        titulo="5 dicas para sua carreira profissional"
-        descricao="Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end"
-      />
+    <BlogCard
+      titulo="Etiqueta e Disciplina na Legião Romana"
+      descricao="Um manual para calouros da Décima Segunda Legião sobre como subir na hierarquia e não irritar o [Terminus](https://riordan.fandom.com)."
+    />
+
+    <BlogCard
+      titulo="O Guia de Culinária do Chalé de Hefesto"
+      descricao="Como usar o fogo das forjas para preparar o melhor marshmallow do acampamento sem causar uma explosão acidental."
+    />
+
 
       <a href="#">Ver tudo</a>
     </section>
--- praticas react/react-app-02/src/components/Profile/Profile.jsx
+++ praticas react/react-app-02/src/components/Profile/Profile.jsx
@@ -3,9 +3,12 @@ import './Profile.css'
 function Profile() {
   return (
     <section className="profile">
-      <img src="perfil.jpeg" alt="perfil" />
-      <h1>Annabeth Chase</h1>
-      <p>Annabeth Chase é uma semideusa grega, filha de Atena e Frederick Chase, e uma das protagonistas centrais na série Percy Jackson e os Olimpianos e Os Heróis do Olimpo, de Rick Riordan. Nascida em 12 de julho, ela é conhecida como a "filha do cérebro" de Atena, possuindo grande inteligência, coragem e habilidade estratégica.</p>
+      <img src= "Perfil.jpeg" alt="perfil" />
+      
+      <div className="profile-text">
+        <h1>Annabeth Chase</h1>
+        <p>Annabeth Chase é uma semideusa grega, filha de Atena e Frederick Chase, e uma das protagonistas centrais na série Percy Jackson e os Olimpianos e Os Heróis do Olimpo, de Rick Riordan. Nascida em 12 de julho, ela é conhecida como a "filha do cérebro" de Atena, possuindo grande inteligência, coragem e habilidade estratégica.</p>
+      </div>
     </section>
   )
 }
