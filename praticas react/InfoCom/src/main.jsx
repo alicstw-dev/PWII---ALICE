446d54b6b12b13dd1439c4936ccdda28532d62c1
Alice <alicemariadelimamelo@gmail.com>
2026-03-10 20:07:54 -0300

    fix: alterando arquivos com imports errados.

--- praticas react/InfoCom/src/main.jsx
+++ praticas react/InfoCom/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { CartProvider } from "./contexts/CartContext";
-import Cart from "./pages/Cart";
 
 const router = createBrowserRouter([
   {
